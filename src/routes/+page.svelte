e579e2df2b8e5098c5e6d7dc25d07255ec556600
MarBGamer <m.battjes91@gmail.com>
2023-01-16 13:32:55 +0100

    ✨ Setup web3 connection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script>
+    import { selectedAccount, chainId, defaultEvmStores } from 'svelte-web3';
+    import { onMount } from 'svelte';
+
+    onMount( defaultEvmStores.setProvider );
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>account: {$selectedAccount} / chainId: {$chainId}</p>
