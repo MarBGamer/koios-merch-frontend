c37110e905374235aeaad61b5f510d9cea967b2b
MarBGamer <m.battjes91@gmail.com>
2023-01-17 15:30:42 +0100

    :sparkles: Setup basic cart system

---
+++ src/lib/cart.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { count } from '$lib/stores';
+
+    let products = [];
+
+    const unsubscribe = count.subscribe((value) => {
+        products = value;
+    });
+</script>
+
+<h1>Cart</h1>
+<ul>
+    {#each products as product}
+        <li>{product.name}</li>
+    {/each}
+</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script>
-    import { selectedAccount, chainId, defaultEvmStores } from 'svelte-web3';
-    import { onMount } from 'svelte';
+    import Cart from '$lib/cart.svelte';
 
-    onMount( defaultEvmStores.setProvider );
+//    import { selectedAccount, chainId, defaultEvmStores } from 'svelte-web3';
+//    import { onMount } from 'svelte';
+
+//    onMount( defaultEvmStores.setProvider );
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Cart />
 
-<p>account: {$selectedAccount} / chainId: {$chainId}</p>
